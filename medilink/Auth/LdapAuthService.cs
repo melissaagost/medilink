f3b5f3104ec5bf98fc68fa9f5f81f00423411f79
melissaagost <melisalezcanoairaldi@gmail.com>
2025-09-14 20:15:19 -0300

    funciona logueo por AD

--- medilink/Auth/LdapAuthService.cs
+++ medilink/Auth/LdapAuthService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.Linq;
 
@@ -16,20 +18,12 @@ namespace medilink.Auth
         private readonly string _grpRec = ConfigurationManager.AppSettings["GrpRecepcionista"];
         private PrincipalContext _principalContext;
 
+
         // Crear el contexto de conexión al dominio
         private PrincipalContext GetContext()
         {
-            //if (!string.IsNullOrWhiteSpace(_domainName))
-            //{
-            //    //_principalContext = new PrincipalContext(ContextType.Domain, _domainName);
-            //    //return _principalContext;
-            //}
-            //return new PrincipalContext(ContextType.Domain, _domainName);
 
 
-            // fallback: usar IP + BaseDN
-
-            //return new PrincipalContext(ContextType.Domain, _adServer, _baseDn);
 
             if (_principalContext == null)
             {
@@ -40,14 +34,7 @@ namespace medilink.Auth
 
         }
 
-        // Validar credenciales contra Active Directory
-        //public bool ValidateUser(string username, string password)
-        //{
-        //    using (var ctx = GetContext())
-        //    {
-        //        return ctx.ValidateCredentials(username, password, ContextOptions.Negotiate);
-        //    }
-        //}
+    
 
         public bool ValidateUser(string username, string password)
         {
@@ -70,31 +57,6 @@ namespace medilink.Auth
         }
 
 
-        // Obtener el rol del usuario según los grupos de AD
-        //public string GetUserRole(string username)
-        //{
-        //    using (var ctx = GetContext())
-        //    using (var user = UserPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, username))
-        //    {
-        //        if (user == null) return null;
-
-        //        var groups = user.GetAuthorizationGroups().Select(g => g.SamAccountName).ToList();
-
-        //        if (groups.Contains(_grpSis, StringComparer.OrdinalIgnoreCase))
-        //            return "Sistemas";
-
-        //        if (groups.Contains(_grpGes, StringComparer.OrdinalIgnoreCase))
-        //            return "Gestor";
-
-        //        if (groups.Contains(_grpMed, StringComparer.OrdinalIgnoreCase))
-        //            return "Medico";
-
-        //        if (groups.Contains(_grpRec, StringComparer.OrdinalIgnoreCase))
-        //            return "Recepcionista";
-
-        //        return null; // No tiene grupo válido de la app
-        //    }
-        //}
 
         public string GetUserRole(string username, string password)
         {
@@ -106,31 +68,35 @@ namespace medilink.Auth
             password))
                 {
                     var up = UserPrincipal.FindByIdentity(ctx, username);
-                    var groups = up.GetAuthorizationGroups().Select(g => g.SamAccountName).ToList();
-                    if (groups.Any(n => n.Equals(_grpSis, StringComparison.OrdinalIgnoreCase))) return "Sistemas";
-                    if (groups.Any(n => n.Equals(_grpGes, StringComparison.OrdinalIgnoreCase))) return "Gestor";
-                    if (groups.Any(n => n.Equals(_grpMed, StringComparison.OrdinalIgnoreCase))) return "Medico";
-                    if (groups.Any(n => n.Equals(_grpRec, StringComparison.OrdinalIgnoreCase))) return "Recepcionista";
-                    return null; // only works if username/password were valid
+                var de = up.GetUnderlyingObject() as DirectoryEntry;
+                var groups = de.Properties["memberOf"];
+
+                var targetGroups = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
+                {
+
+                    _grpSis, _grpGes, _grpMed, _grpRec
+                };
+
+                var groupRoleMap = new Dictionary<string, string>
+                {
+                    { _grpSis,"Sistemas" },
+                    { _grpGes, "Gestor" },
+                    { _grpMed, "Medico" },
+                    { _grpRec, "Recepcionista"}
+                };
+
+                foreach (var group in groups)
+                {
+                    foreach (var mygroup in targetGroups)
+                    {
+                        if (group.ToString().ToLower().Contains(mygroup.ToString().ToLower()))
+                        {
+                            return groupRoleMap[mygroup.ToString()];
+                        }
+                    }
                 }
-            
-            //var ctx = GetContext();
-
-            ////{
-            //    //var up =
-            //    //    UserPrincipal.FindByIdentity(ctx, IdentityType.SamAccountName, username)
-            //    // ?? UserPrincipal.FindByIdentity(ctx, IdentityType.UserPrincipalName, $"{username}@LU60591");
-
-            //    var up = UserPrincipal.FindByIdentity(ctx, username);
-            //    if (up == null) return null;
-
-                //var groups = up.GetAuthorizationGroups().Select(g => g.SamAccountName).ToList();
-                //if (groups.Any(n => n.Equals(_grpSis, StringComparison.OrdinalIgnoreCase))) return "Sistemas";
-                //if (groups.Any(n => n.Equals(_grpGes, StringComparison.OrdinalIgnoreCase))) return "Gestor";
-                //if (groups.Any(n => n.Equals(_grpMed, StringComparison.OrdinalIgnoreCase))) return "Medico";
-                //if (groups.Any(n => n.Equals(_grpRec, StringComparison.OrdinalIgnoreCase))) return "Recepcionista";
-                //return null;
-            //}
+                return null; 
+                }
         }
 
     }
